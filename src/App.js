d0e0f7477e76e68b516b3c92c60982108c123668
jinsil <developer_corn@daum.net>
2023-04-06 23:36:42 +0900

    study about style in react 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
 
 function App() {
   return (
     <div className="App">
-      <SassComponent />
+      <CSSModule />
     </div>
   );
 }
--- src/SassComponent.js
+++ src/SassComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/SassComponent.scss';
+import './SassComponent.scss';
 
 const SassComponent = () => {
     return (
